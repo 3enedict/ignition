8629ec541dbf3097b6afcd203b99127e8723a2db
BPN06 <benoit.philip.poitevin@gmail.com>
2021-12-11 21:50:08 +0100

    Refactored render pass

--- src/renderer/setup.rs
+++ src/renderer/setup.rs
@@ -32,6 +32,9 @@ use logical_device::VglLogicalDevice;
 pub mod swapchain;
 use swapchain::VglSwapchain;
 
+pub mod render_pass;
+use render_pass::VglRenderPass;
+
 
 fn window_size_dependent_setup(
     images: &[Arc<SwapchainImage<Window>>],
@@ -97,7 +100,7 @@ impl VglRenderer {
         #[derive(Default, Debug, Clone)]
         struct Vertex {
             position: [f32; 2],
-            }
+        }
         vulkano::impl_vertex!(Vertex, position);
 
         let vertex_buffer = CpuAccessibleBuffer::from_iter(
@@ -132,48 +135,48 @@ impl VglRenderer {
             vulkano_shaders::shader! {
                 ty: "vertex",
                 src: "
-#version 450
-#extension GL_ARB_separate_shader_objects : enable
+    #version 450
+    #extension GL_ARB_separate_shader_objects : enable
 
-out gl_PerVertex {
-    vec4 gl_Position;
-};
+    out gl_PerVertex {
+        vec4 gl_Position;
+    };
 
-layout(location = 0) in vec2 position;
+    layout(location = 0) in vec2 position;
 
-layout(location = 0) out vec3 fragColor;
+    layout(location = 0) out vec3 fragColor;
 
-vec3 colors[3] = vec3[](
-    vec3(1.0, 0.0, 0.0),
-    vec3(0.0, 1.0, 0.0),
-    vec3(0.0, 0.0, 1.0)
-);
+    vec3 colors[3] = vec3[](
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0)
+    );
 
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-    fragColor = colors[gl_VertexIndex];
-}
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+        fragColor = colors[gl_VertexIndex];
+    }
       "
             }
-            }
+        }
 
         mod fs {
             vulkano_shaders::shader! {
                 ty: "fragment",
                 src: "
-#version 450
-#extension GL_ARB_separate_shader_objects : enable
+    #version 450
+    #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) in vec3 fragColor;
+    layout(location = 0) in vec3 fragColor;
 
-layout(location = 0) out vec4 outColor;
+    layout(location = 0) out vec4 outColor;
 
-void main() {
-    outColor = vec4(fragColor, 1.0);
-}
+    void main() {
+        outColor = vec4(fragColor, 1.0);
+    }
             "
             }
-            }
+        }
 
         let vs = vs::Shader::load(logical_device.clone_logical_device()).unwrap();
         let fs = fs::Shader::load(logical_device.clone_logical_device()).unwrap();
@@ -185,35 +188,9 @@ void main() {
         // The next step is to create a *render pass*, which is an object that describes where the
         // output of the graphics pipeline will go. It describes the layout of the images
         // where the colors, depth and/or stencil information will be written.
-        let render_pass = Arc::new(
-            vulkano::single_pass_renderpass!(
-                logical_device.clone_logical_device(),
-                attachments: {
-                    // `color` is a custom name we give to the first and only attachment.
-                    color: {
-                        // `load: Clear` means that we ask the GPU to clear the content of this
-                        // attachment at the start of the drawing.
-                        load: Clear,
-                        // `store: Store` means that we ask the GPU to store the output of the draw
-                        // in the actual image. We could also ask it to discard the result.
-                        store: Store,
-                        // `format: <ty>` indicates the type of the format of the image. This has to
-                        // be one of the types of the `vulkano::format` module (or alternatively one
-                        // of your structs that implements the `FormatDesc` trait). Here we use the
-                        // same format as the swapchain.
-                        format: swapchain.get_swapchain().format(),
-                        // TODO:
-                        samples: 1,
-                    }
-                },
-                pass: {
-                    // We use the attachment named `color` as the one and only color attachment.
-                    color: [color],
-                    // No depth-stencil attachment is indicated with empty brackets.
-                    depth_stencil: {}
-            }
-        )
-            .unwrap(),
+        let render_pass = VglRenderPass::new(
+            &logical_device,
+            &swapchain,
         );
 
         // Before we draw we have to create what is called a pipeline. This is similar to an OpenGL
@@ -236,7 +213,7 @@ void main() {
             .fragment_shader(fs.main_entry_point(), ())
             // We have to indicate which subpass of which render pass this pipeline is going to be used
             // in. The pipeline will only be usable from this particular subpass.
-            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .render_pass(Subpass::from(render_pass.clone_render_pass(), 0).unwrap())
             // Now that our builder is filled, we call `build()` to obtain an actual pipeline.
             .build(logical_device.clone_logical_device())
             .unwrap(),
@@ -255,7 +232,7 @@ void main() {
         //
         // Since we need to draw to multiple images, we are going to create a different framebuffer for
         // each image.
-        let mut framebuffers = window_size_dependent_setup(swapchain.get_images(), render_pass.clone(), &mut viewport);
+        let mut framebuffers = window_size_dependent_setup(swapchain.get_images(), render_pass.clone_render_pass(), &mut viewport);
 
         // Initialization is finally finished!
 
@@ -310,7 +287,7 @@ void main() {
                         // recreate framebuffers as well.
                         framebuffers = window_size_dependent_setup(
                             swapchain.get_images(),
-                            render_pass.clone(),
+                            render_pass.clone_render_pass(),
                             &mut viewport,
                         );
                         recreate_swapchain = false;
---
+++ src/renderer/setup/render_pass.rs
@@ -0,0 +1,48 @@
+use std::sync::Arc;
+
+use vulkano::render_pass::RenderPass;
+
+use crate::renderer::setup::VglLogicalDevice;
+use crate::renderer::setup::VglSwapchain;
+
+pub struct VglRenderPass {
+    render_pass: Arc<RenderPass>,
+}
+
+impl VglRenderPass {
+    pub fn new(
+        logical_device: &VglLogicalDevice,
+        swapchain: &VglSwapchain,
+    ) -> Self {
+        let render_pass = Arc::new(
+            vulkano::single_pass_renderpass!(
+                logical_device.clone_logical_device(),
+                attachments: {
+                    color: {
+                        load: Clear,
+                        store: Store,
+                        format: swapchain.get_swapchain().format(),
+                        samples: 1,
+                    }
+                },
+                pass: {
+                    color: [color],
+                    depth_stencil: {}
+            }
+        )
+            .unwrap(),
+        );
+
+        Self {
+            render_pass,
+        }
+    }
+
+    pub fn get_render_pass(&self) -> &Arc<RenderPass> {
+        &self.render_pass
+    }
+
+    pub fn clone_render_pass(&self) -> Arc<RenderPass> {
+        self.render_pass.clone()
+    }
+}
