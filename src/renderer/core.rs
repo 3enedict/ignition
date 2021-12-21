4d7cc9c0b8d8513d70821d0e234fed8c28dd1005
BPN06 <benoit.philip.poitevin@gmail.com>
2021-12-21 10:43:30 +0100

    Updated shaders with a rather hacky solution...

--- src/renderer/core.rs
+++ src/renderer/core.rs
@@ -47,10 +47,21 @@ mod vs {
         ty: "vertex",
         src: "
     #version 450
+    #extension GL_ARB_separate_shader_objects : enable
+
+    layout(location = 0) out vec3 fragColor;
+
     layout(location = 0) in vec2 position;
+
+    vec3 colors[3] = vec3[](
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0)
+    );
 
     void main() {
         gl_Position = vec4(position, 0.0, 1.0);
+        fragColor = colors[gl_VertexIndex % 3];
     }
       "
     }
@@ -61,11 +72,14 @@ mod fs {
         ty: "fragment",
         src: "
     #version 450
+    #extension GL_ARB_separate_shader_objects : enable
+
+    layout(location = 0) in vec3 fragColor;
 
     layout(location = 0) out vec4 outColor;
 
     void main() {
-        outColor = vec4(0.75, 0.75, 0.75, 0.5);
+        outColor = vec4(fragColor, 1.0);
     }
             "
     }
