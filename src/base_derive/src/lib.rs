3bf20787126098992470f972d3b90649d7704626
BPN06 <benoit.philip.poitevin@gmail.com>
2022-01-24 19:10:36 +0100

    Added the possibility to find the renderer either in the main class or the view for the MVC pattern

--- src/base_derive/src/lib.rs
+++ src/base_derive/src/lib.rs
@@ -2,17 +2,19 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::spanned::Spanned;
 use syn;
 
 #[proc_macro_derive(Base)]
-pub fn base_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
+pub fn base_derive(raw_input: TokenStream) -> TokenStream {
+    let input = syn::parse(raw_input).unwrap();
 
-    impl_base(&ast)
+    impl_base(&input)
 }
 
-fn impl_base(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
+fn impl_base(input: &syn::DeriveInput) -> TokenStream {
+    let name = &input.ident;
+    let renderer = find_renderer(&input.data);
 
     let gen = quote! {
         use winit::event_loop::{EventLoop, ControlFlow};
@@ -47,10 +49,36 @@ fn impl_base(ast: &syn::DeriveInput) -> TokenStream {
             }
 
             fn get_renderer(&mut self) -> &mut VglRenderer {
-                &mut self.view.renderer
+                &mut self.#renderer
             }
         }
     };
 
     gen.into()
+}
+
+fn find_renderer(data: &syn::Data) -> proc_macro2::TokenStream {
+    match *data {
+        syn::Data::Struct(ref data) => {
+            match data.fields {
+                syn::Fields::Named(ref fields) => {
+                    let mut renderer = proc_macro2::TokenStream::new();
+
+                    for field in fields.named.iter() {
+                        let name = &field.ident;
+
+                        if name == &Some(syn::Ident::new("view", name.span())) {
+                            renderer = quote!{view.renderer};
+                        } else if name == &Some(syn::Ident::new("renderer", name.span())) {
+                            renderer = quote!{renderer};
+                        }
+                    }
+
+                    renderer
+                }
+                syn::Fields::Unit | syn::Fields::Unnamed(_) => unimplemented!(),
+            }
+        }
+        syn::Data::Enum(_) | syn::Data::Union(_) => unimplemented!(),
+    }
 }
