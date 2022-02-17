ebf94c0479bf763c0a6d96a015e2b7c63581d7c5
BPN06 <benoit.philip.poitevin@gmail.com>
2022-02-17 18:21:04 +0100

    Refactored IgnitionWindow and IgnitionGPU

--- src/core/rendering/gpu.rs
+++ src/core/rendering/gpu.rs
@@ -23,31 +23,38 @@ pub struct IgnitionGPU {
 
 impl IgnitionGPU {
     pub async fn new(instance: &Instance, window: &IgnitionWindow) -> Self {
-        let adapter = instance.request_adapter(
-            &RequestAdapterOptions {
-                power_preference: PowerPreference::default(),
-                compatible_surface: Some(&window.surface),
-                force_fallback_adapter: false,
-            },
-        ).await.unwrap();
+        let adapter = pollster::block_on(Self::get_adapter(instance, window));
 
-        let adapter_info = adapter.get_info();
-        println!("Device name : {}", adapter_info.name);
+        println!("Device name : {}", adapter.get_info().name);
 
-        let (device, queue) = adapter.request_device(
-            &DeviceDescriptor {
-                features: Features::empty(),
-                limits: Limits::default(),
-                label: None,
-            },
-            None,
-        ).await.unwrap();
-        
+        let (device, queue) = pollster::block_on(Self::get_device(&adapter));
+
         Self {
             adapter,
 
             device,
             queue,
         }
+    }
+
+    async fn get_adapter(instance: &Instance, window: &IgnitionWindow) -> Adapter {
+        instance.request_adapter(
+            &RequestAdapterOptions {
+                power_preference: PowerPreference::default(),
+                compatible_surface: Some(&window.surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap()
+    }
+
+    async fn get_device(adapter: &Adapter) -> (Device, Queue) {
+        adapter.request_device(
+            &DeviceDescriptor {
+                features: Features::empty(),
+                limits: Limits::default(),
+                label: None,
+            },
+            None,
+        ).await.unwrap()
     }
 }
--- src/core/rendering/window.rs
+++ src/core/rendering/window.rs
@@ -28,22 +28,9 @@ pub struct IgnitionWindow {
 
 impl IgnitionWindow {
     pub fn new(instance: &Instance) -> Self {
-        let event_loop = EventLoop::new();
-        let window = WindowBuilder::new()
-            .build(&event_loop)
-            .unwrap();
-
-        let size = window.inner_size();
-
-        let surface = unsafe { instance.create_surface(&window) };
-
-        let config = SurfaceConfiguration {
-            usage: TextureUsages::RENDER_ATTACHMENT,
-            format: TextureFormat::Bgra8UnormSrgb,
-            width: size.width,
-            height: size.height,
-            present_mode: PresentMode::Fifo,
-        };
+        let (event_loop, window, size) = Self::create_window();
+        let surface = Self::create_surface(&instance, &window);
+        let config = Self::generate_default_configuration(&size);
 
         Self { 
             event_loop: Some(event_loop), 
@@ -53,6 +40,31 @@ impl IgnitionWindow {
 
             surface,
             config,
+        }
+    }
+
+    fn create_window() -> (EventLoop<()>, Window, PhysicalSize<u32>) {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
+            .build(&event_loop)
+            .unwrap();
+
+        let size = window.inner_size();
+
+        (event_loop, window, size)
+    }
+
+    fn create_surface(instance: &Instance, window: &Window) -> Surface {
+        unsafe { instance.create_surface(&window) }
+    }
+
+    fn generate_default_configuration(size: &PhysicalSize<u32>) -> SurfaceConfiguration {
+        SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT,
+            format: TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: PresentMode::Fifo,
         }
     }
 }
