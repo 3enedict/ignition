b112455807200922d693cc6c5c80efeb6db4cdef
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-07-11 17:33:37 +0200

    refactor: Removed unecessary tests

--- src/life/genesis/component.rs
+++ src/life/genesis/component.rs
@@ -34,7 +34,7 @@ impl Scene {
 impl<G> ComponentPool<G> {
     pub fn assign_component(&mut self, entity: usize, component: G) {
         if self.has_component(entity) {
-            self.component_array[self.sparse_array[entity] as usize] = component;
+            *self.get_mut(entity) = component;
         } else {
             Self::add_entity_to_sparse_array(entity, self.num_components, &mut self.sparse_array);
 
@@ -47,7 +47,7 @@ impl<G> ComponentPool<G> {
 
 #[cfg(test)]
 mod tests {
-    use crate::life::{genesis::entity::EntityConstructor, ComponentPool, Scene};
+    use crate::life::Scene;
 
     #[test]
     fn creating_new_component_pool_updates_scene() {
@@ -87,40 +87,6 @@ mod tests {
     }
 
     #[test]
-    fn assigning_component_updates_component_pool() {
-        let mut pool = ComponentPool::new_with_entity(3, 32);
-        pool.assign_component(6, 28);
-
-        assert_eq!(
-            pool,
-            ComponentPool {
-                num_components: 2,
-
-                sparse_array: vec![-1, -1, -1, 0, -1, -1, 1],
-                packed_array: vec![3, 6],
-                component_array: vec![32, 28],
-            },
-        );
-    }
-
-    #[test]
-    fn assigning_already_existing_component_does_not_add_component() {
-        let mut pool = ComponentPool::new_with_entity(0, 32);
-        pool.assign_component(0, 28);
-
-        assert_eq!(
-            pool,
-            ComponentPool {
-                num_components: 1,
-
-                sparse_array: vec![0],
-                packed_array: vec![0],
-                component_array: vec![28],
-            },
-        );
-    }
-
-    #[test]
     fn creating_vectorized_component_encapsulates_it_in_vector() {
         let mut scene = Scene::new();
 
@@ -134,41 +100,8 @@ mod tests {
     fn adding_to_vectorized_component_pushes_to_vector() {
         let mut scene = Scene::new();
 
-        let entity = scene.entity();
-        scene.vectorized_component(entity, 34 as i32);
-        scene.vectorized_component(entity, 59 as i32);
-
-        assert_eq!(
-            scene.get::<Vec<i32>>().iter().collect::<Vec<&Vec<i32>>>(),
-            vec![&vec![34, 59]]
-        );
-    }
-
-    #[test]
-    fn adding_to_second_vectorized_component_pushes_to_vector() {
-        let mut scene = Scene::new();
-
         let entity1 = scene.entity();
         scene.vectorized_component(entity1, 34 as i32);
-        scene.vectorized_component(entity1, 59 as i32);
-
-        let entity2 = scene.entity();
-        scene.vectorized_component(entity2, 63 as i32);
-        scene.vectorized_component(entity2, 16 as i32);
-
-        assert_eq!(
-            scene.get::<Vec<i32>>().iter().collect::<Vec<&Vec<i32>>>(),
-            vec![&vec![34, 59], &vec![63, 16]]
-        );
-    }
-
-    #[test]
-    fn adding_differently_typed_vectorized_components_does_not_crash() {
-        let mut scene = Scene::new();
-
-        let entity1 = scene.entity();
-        scene.vectorized_component(entity1, 34 as i32);
-        scene.vectorized_component(entity1, 0.59 as f32);
         scene.vectorized_component(entity1, 81 as i32);
 
         let entity2 = scene.entity();
@@ -178,11 +111,6 @@ mod tests {
         assert_eq!(
             scene.get::<Vec<i32>>().iter().collect::<Vec<&Vec<i32>>>(),
             vec![&vec![34, 81]]
-        );
-
-        assert_eq!(
-            scene.get::<Vec<f32>>().iter().collect::<Vec<&Vec<f32>>>(),
-            vec![&vec![0.59]]
         );
 
         assert_eq!(
