8987405c44a2c47a63b462932f3f785c5fc8a7b5
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-07-19 19:39:24 +0200

    feature: No need to reference data in VertexGroup

--- src/manifestation/apex.rs
+++ src/manifestation/apex.rs
@@ -1,14 +1,14 @@
 use wgpu::{BufferAddress, VertexAttribute, VertexBufferLayout, VertexFormat, VertexStepMode};
 
-pub struct VertexGroup<'a> {
-    data: Vec<Vec<&'a [u8]>>,
+pub struct VertexGroup {
+    data: Vec<Vec<Vec<u8>>>,
 
     stride: usize,
     shader_location: u32,
     layout: Vec<VertexAttribute>,
 }
 
-impl<'a> VertexGroup<'a> {
+impl VertexGroup {
     pub fn new() -> Self {
         Self {
             data: Vec::new(),
@@ -19,7 +19,12 @@ impl<'a> VertexGroup<'a> {
         }
     }
 
-    pub fn data<G: bytemuck::Pod>(&mut self, data: &'a [G], step: usize, format: VertexFormat) {
+    pub fn data<G: serde::ser::Serialize, const N: usize>(
+        &mut self,
+        data: [G; N],
+        step: usize,
+        format: VertexFormat,
+    ) {
         self.layout.push(VertexAttribute {
             offset: self.stride as BufferAddress,
             shader_location: self.shader_location,
@@ -35,7 +40,7 @@ impl<'a> VertexGroup<'a> {
             }
 
             for value in point.into_iter() {
-                self.data[i].push(bytemuck::bytes_of(value));
+                self.data[i].push(bincode::serialize(value).unwrap());
             }
         }
     }
@@ -51,7 +56,6 @@ impl<'a> VertexGroup<'a> {
 
 #[cfg(test)]
 mod tests {
-    use bytemuck::bytes_of;
     use wgpu::{BufferAddress, VertexAttribute, VertexBufferLayout, VertexFormat, VertexStepMode};
 
     use crate::manifestation::apex::VertexGroup;
@@ -60,7 +64,7 @@ mod tests {
     fn vertex_data_gets_seperated_and_casted_correctly() {
         let mut vertex_group = VertexGroup::new();
         vertex_group.data(
-            &[0.55, -0.5, 0.55, 0.55, -0.5, 0.55],
+            [0.55, -0.5, 0.55, 0.55, -0.5, 0.55],
             2,
             VertexFormat::Float32x2,
         );
@@ -68,9 +72,18 @@ mod tests {
         assert_eq!(
             vertex_group.data,
             vec![
-                vec![bytes_of(&0.55), bytes_of(&-0.5)],
-                vec![bytes_of(&0.55), bytes_of(&0.55)],
-                vec![bytes_of(&-0.5), bytes_of(&0.55)]
+                vec![
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&-0.5).unwrap()
+                ],
+                vec![
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&0.55).unwrap()
+                ],
+                vec![
+                    bincode::serialize(&-0.5).unwrap(),
+                    bincode::serialize(&0.55).unwrap()
+                ]
             ]
         );
     }
@@ -79,12 +92,12 @@ mod tests {
     fn different_types_of_vertex_data_get_processed_correctly() {
         let mut vertex_group = VertexGroup::new();
         vertex_group.data(
-            &[0.55, -0.5, 0.55, 0.55, -0.5, 0.55],
+            [0.55, -0.5, 0.55, 0.55, -0.5, 0.55],
             2,
             VertexFormat::Float32x2,
         );
         vertex_group.data(
-            &[1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0],
             3,
             VertexFormat::Float32x3,
         );
@@ -93,25 +106,25 @@ mod tests {
             vertex_group.data,
             vec![
                 vec![
-                    bytes_of(&0.55),
-                    bytes_of(&-0.5),
-                    bytes_of(&1.0),
-                    bytes_of(&0.0),
-                    bytes_of(&0.0)
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&-0.5).unwrap(),
+                    bincode::serialize(&1.0).unwrap(),
+                    bincode::serialize(&0.0).unwrap(),
+                    bincode::serialize(&0.0).unwrap()
                 ],
                 vec![
-                    bytes_of(&0.55),
-                    bytes_of(&0.55),
-                    bytes_of(&0.0),
-                    bytes_of(&1.0),
-                    bytes_of(&0.0)
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&0.0).unwrap(),
+                    bincode::serialize(&1.0).unwrap(),
+                    bincode::serialize(&0.0).unwrap()
                 ],
                 vec![
-                    bytes_of(&-0.5),
-                    bytes_of(&0.55),
-                    bytes_of(&0.0),
-                    bytes_of(&0.0),
-                    bytes_of(&1.0)
+                    bincode::serialize(&-0.5).unwrap(),
+                    bincode::serialize(&0.55).unwrap(),
+                    bincode::serialize(&0.0).unwrap(),
+                    bincode::serialize(&0.0).unwrap(),
+                    bincode::serialize(&1.0).unwrap()
                 ],
             ]
         );
@@ -121,7 +134,7 @@ mod tests {
     fn layout_is_generated_correctly() {
         let mut vertex_group = VertexGroup::new();
         vertex_group.data(
-            &[
+            [
                 0.55 as f32,
                 -0.5 as f32,
                 0.55 as f32,
@@ -133,7 +146,7 @@ mod tests {
             VertexFormat::Float32x2,
         );
         vertex_group.data(
-            &[
+            [
                 1.0 as f32, 0.0 as f32, 0.0 as f32, 0.0 as f32, 1.0 as f32, 0.0 as f32, 0.0 as f32,
                 0.0 as f32, 1.0 as f32,
             ],
