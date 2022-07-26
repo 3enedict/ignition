4a98c390dd69936295a546b6625ad484b35f6943
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-07-26 19:36:31 +0200

    test: Added get_mut tests

--- src/life/abduction.rs
+++ src/life/abduction.rs
@@ -155,4 +155,33 @@ mod tests {
             Ok(_) => panic!("Error was not propagated successfully from get_trait() to get()"),
         }
     }
+
+    #[test]
+    fn unable_to_downcast_component_pool_trait_returns_error_mut_version() {
+        let mut scene = Scene::new();
+
+        let type_id = TypeId::of::<f32>();
+        let component_pool = Box::new(ComponentPool::new_with_entity(1, 32 as i32));
+        scene.component_pools.insert(type_id, component_pool);
+
+        match scene.get::<f32>() {
+            Err(e) => assert_eq!(e, LifeError::Downcast(String::from("f32"))),
+            Ok(_) => panic!(
+                "Scene should not have been able to downcast Box<dyn ComponentPoolTrait> for f32"
+            ),
+        }
+    }
+
+    #[test]
+    fn no_component_pool_error_is_correctly_propagated_at_get_mut() {
+        let mut scene = Scene::new();
+
+        let entity = scene.entity();
+        scene.component(entity, 1 as i32);
+
+        match scene.get::<f32>() {
+            Err(e) => assert_eq!(e, LifeError::NoComponentPool(String::from("f32"))),
+            Ok(_) => panic!("Error was not propagated successfully from get_trait() to get()"),
+        }
+    }
 }
